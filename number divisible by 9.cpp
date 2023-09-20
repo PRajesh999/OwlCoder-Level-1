ced951bef63861dc8f9f28e8b6de3dc736fa51eb
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-09-20 22:05:20 +0530

    Add files via upload

---
+++ number divisible by 9.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%9==0)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+}
