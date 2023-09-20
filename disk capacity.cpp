64a688b107a76b9064dfe1d6347a700d8ebc4a0c
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-09-20 20:26:42 +0530

    Add files via upload

---
+++ disk capacity.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=a*b*c*512*2;
+    cout<<d;   
+}
---
+++ expenditure.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+       int x,y;
+       cin>>x>>y;
+       int z=30*y;
+       if(z<=x) cout<<"YES";
+       else cout<<"NO";
+}
