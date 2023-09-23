c4b034e5b074ffb1c9c6f9950d5114b98c428d3b
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-09-23 21:24:01 +0530

    Add files via upload

---
+++ contests.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,z;
+    cin>>x>>y>>z;
+    int w=((y*1)+(z*2));
+    if(w>=x) cout<<"Qualify";
+    else cout<<"Not Qualify";
+}
